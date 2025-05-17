906535922444bf8f589372b0df36d9a795e56e37
Lai Hong Yu <joyjay606@gmail.com>
2025-05-17 15:45:18 +0800

    Initial Commit

---
+++ plants/plantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantsApp.swift
+//  plants
+//
+//  Created by Lai Hong Yu on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct plantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
